9c9f8d395a551fd71cc8da1230aa5ad24ea64a9c
Rune Nielsen <runenielsen@runbox.com>
2022-12-03 19:48:05 +0100

    simplified solution

--- src/day_1/core.clj
+++ src/day_1/core.clj
@@ -1,10 +1,9 @@
 (ns day-1.core
   (:require [clojure.string :as str]))
 
-(defn highest-total-calories [list]
-  (->> (str/split list #"\n\n")
-       (map #(str/split % #"\n"))
-       (map (fn [xs] (map #(Integer/parseInt %) xs)))
-       (mapv #(apply + %))
-       (sort)
-       (last)))
+(defn highest-total-calories [calorie-list]
+  (->> (str/split calorie-list #"\n\n")
+       (map #(map (fn [s] (Integer/parseInt s)) (str/split % #"\n")))
+       (map #(apply + %))
+       (sort >)
+       (first)))
