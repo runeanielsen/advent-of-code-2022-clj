7f0b7944c4b7b10ce298151106c464def95bb1bf
Rune Nielsen <runenielsen@runbox.com>
2022-12-03 20:35:43 +0100

    solve question 2 for day 1

--- src/day_1/core.clj
+++ src/day_1/core.clj
@@ -1,9 +1,10 @@
 (ns day-1.core
   (:require [clojure.string :as str]))
 
-(defn highest-total-calories [calorie-list]
+(defn highest-total-calories [calorie-list n]
   (->> (str/split calorie-list #"\n\n")
        (map #(map (fn [s] (Integer/parseInt s)) (str/split % #"\n")))
        (map #(apply + %))
        (sort >)
-       (first)))
+       (take n)
+       (apply +)))
--- src/day_1/tests.clj
+++ src/day_1/tests.clj
@@ -1,8 +1,14 @@
 (ns day-1.tests
   (:require [day-1.core :refer [highest-total-calories]]
-            [clojure.test :refer [deftest is]]
+            [clojure.test :refer [deftest is testing]]
             [clojure.java.io :as io]))
 
+;; First star question
 (deftest highest-total-calories-returns-highest-total
-  (let [input (slurp (io/resource "day_1/calorie-list.txt"))]
-    (is (= (highest-total-calories input) 71502))))
+  (testing "Getting highest elf output."
+    (let [input (slurp (io/resource "day_1/calorie-list.txt"))]
+      (is (= (highest-total-calories input 1) 71502))))
+
+  (testing "Getting top three elf output."
+    (let [input (slurp (io/resource "day_1/calorie-list.txt"))]
+      (is (= (highest-total-calories input 3) 208191)))))
